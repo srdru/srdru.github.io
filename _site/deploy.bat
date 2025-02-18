3d8071dd9439a3bb42fe03a0f7985ffba5075f51
srdru <serdaruslu89@gmail.com>
2025-02-18 19:09:13 +0300

    srdru.github.io

--- _site/deploy.bat
+++ _site/deploy.bat
@@ -1,33 +1,8 @@
 @echo off
 call bundle exec jekyll build
-if %errorlevel% neq 0 (
-    echo Jekyll build hata verdi.
-    pause
-    exit /b %errorlevel%
-)
 git pull origin main
-if %errorlevel% neq 0 (
-    echo Git pull hata verdi.
-    pause
-    exit /b %errorlevel%
-)
 git add .
-if %errorlevel% neq 0 (
-    echo Git add hata verdi.
-    pause
-    exit /b %errorlevel%
-)
 git commit -m "srdru.github.io"
-if %errorlevel% neq 0 (
-    echo Git commit hata verdi.
-    pause
-    exit /b %errorlevel%
-)
 git push origin main
-if %errorlevel% neq 0 (
-    echo Git push hata verdi.
-    pause
-    exit /b %errorlevel%
-)
 pause
 
