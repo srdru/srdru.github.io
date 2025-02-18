fa9d0543d7f0d1a00f91a372396eb62eb9de94b8
srdru <serdaruslu89@gmail.com>
2025-02-18 19:03:10 +0300

    srdru.github.io

--- deploy.bat
+++ deploy.bat
@@ -1,32 +1,31 @@
 @echo off
-cd /d C:\Users\username\my-jekyll-site
 call bundle exec jekyll build
 if %errorlevel% neq 0 (
-    echo Jekyll build hata verdi, konsol kapanmıyor.
+    echo Jekyll build hata verdi.
     pause
     exit /b %errorlevel%
 )
 git pull origin main
 if %errorlevel% neq 0 (
-    echo Git pull hata verdi, konsol kapanmıyor.
+    echo Git pull hata verdi.
     pause
     exit /b %errorlevel%
 )
 git add .
 if %errorlevel% neq 0 (
-    echo Git add hata verdi, konsol kapanmıyor.
+    echo Git add hata verdi.
     pause
     exit /b %errorlevel%
 )
 git commit -m "srdru.github.io"
 if %errorlevel% neq 0 (
-    echo Git commit hata verdi, konsol kapanmıyor.
+    echo Git commit hata verdi.
     pause
     exit /b %errorlevel%
 )
 git push origin main
 if %errorlevel% neq 0 (
-    echo Git push hata verdi, konsol kapanmıyor.
+    echo Git push hata verdi.
     pause
     exit /b %errorlevel%
 )
